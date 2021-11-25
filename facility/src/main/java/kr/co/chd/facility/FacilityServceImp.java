1e8eaf2050218f270192e6d6e6b3da2e7b2470cf
sdm05 <sdm05100@naver.com>
2021-11-25 09:23:37 +0900

    관리시설제어 수신 테스트

--- facility/src/main/java/kr/co/chd/facility/FacilityServceImp.java
+++ facility/src/main/java/kr/co/chd/facility/FacilityServceImp.java
@@ -1,5 +1,7 @@
 package kr.co.chd.facility;
 
+import com.pi4j.component.motor.impl.GpioStepperMotorComponent;
+import com.pi4j.io.gpio.*;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +13,56 @@ public class FacilityServceImp implements FacilityService{
     }
 
     @Override
-    public void controlFacility(EnvirInfo envirInfo) {
+    public void controlFacility(EnvirInfo envirInfo) throws InterruptedException {
         int verticalAngle = envirInfo.getVerticalAngle();//세로축 각도
         int horizontalAngle = envirInfo.getVerticalAngle();
 
         System.out.println("세로축 : " + verticalAngle +"\n 가로축"+ horizontalAngle);
+
+        final GpioController gpio = GpioFactory.getInstance();
+
+        final GpioPinDigitalOutput[] pins = {
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW),
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, PinState.LOW),
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, PinState.LOW),
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, PinState.LOW) };
+
+        gpio.setShutdownOptions(true, PinState.LOW, pins);
+
+        GpioStepperMotorComponent motor = new GpioStepperMotorComponent(pins);
+
+        byte[] single_step_sequence = new byte[4];
+        single_step_sequence[0] = (byte) 0b0001;
+        single_step_sequence[1] = (byte) 0b0010;
+        single_step_sequence[2] = (byte) 0b0100;
+        single_step_sequence[3] = (byte) 0b1000;
+
+        motor.setStepInterval(2); // 움직이는 시간 간격 2초가 제일 빠르고 뒤로 갈 수록 느려짐
+        motor.setStepSequence(single_step_sequence); //스텝 방식 설정
+
+        motor.setStepsPerRevolution(2038);//1바퀴를 360도 (2038)
+
+
+        System.out.println("   Motor FORWARD for 2038 steps.");
+        motor.step(2038);//좌로 1바퀴
+        System.out.println("   Motor STOPPED for 2 seconds.");
+        Thread.sleep(2000);
+
+
+        System.out.println("   Motor REVERSE for 2038 steps.");
+        motor.step(-2038);//우로 1바퀴
+        System.out.println("   Motor STOPPED for 2 seconds.");
+        Thread.sleep(2000);
+
+
+        // final stop to ensure no motor activity
+        motor.stop();
+
+        // stop all GPIO activity/threads by shutting down the GPIO controller
+        // (this method will forcefully shutdown all GPIO monitoring threads and
+        // scheduled tasks)
+        gpio.shutdown();
+
     }
 
     @Override
--- facility/src/main/java/kr/co/chd/facility/FacilityService.java
+++ facility/src/main/java/kr/co/chd/facility/FacilityService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public interface FacilityService {
     public EnvirInfo receiveFacilityInfo(EnvirInfo envirInfo);
-    public void controlFacility(EnvirInfo envirInfo);
+    public void controlFacility(EnvirInfo envirInfo) throws InterruptedException;
     public CropAnalysis analysisCrop();
     public void sendCropInfo(CropAnalysis cropAnalysis);
 }
