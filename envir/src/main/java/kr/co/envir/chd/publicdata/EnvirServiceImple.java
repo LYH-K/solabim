e8cc531024aca30a32ab987cb7c1be1641f8c2e5
ydj29 <ydj9072@naver.com>
2021-11-26 21:35:04 +0900

    envir update(2021-11-26).pull

---
+++ envir/src/main/java/kr/co/envir/chd/publicdata/EnvirServiceImple.java
@@ -0,0 +1,35 @@
+package kr.co.envir.chd.publicdata;
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class EnvirServiceImple {
+	public void get() {
+		String url = "http://172.16.30.7/chd/analysis/control";
+
+		try {
+			OkHttpClient client = new OkHttpClient();
+			Request request = new Request.Builder()
+					.url(url)
+					.build();
+
+			//비동기 처리 (enqueue 사용)
+			client.newCall(request).enqueue(new Callback() {
+				//비동기 처리를 위해 Callback 구현
+				@Override
+				public void onFailure(Call call, IOException e) {
+					System.out.println("error + Connect Server Error is " + e.toString());
+				}
+
+				@Override
+				public void onResponse(Call call, Response response) throws IOException {
+					System.out.println("Response Body is " + response.body().string());
+				}
+			});
+
+		} catch (Exception e){
+			System.err.println(e.toString());
+		}
+	}
+}
